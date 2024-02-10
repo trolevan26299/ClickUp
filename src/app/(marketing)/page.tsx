92b522c1502201e96c04baa0fda8c04f1a53333f
andrew <trolevan26299@gmail.com>
2024-02-10 17:15:10 +0700

    update code

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import HomeLoginButton from "./components/HomeLoginButton";
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center text-red-500">
+      <p>Hello build product fast</p>
+      <HomeLoginButton />
+    </main>
+  );
+}
